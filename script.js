1bec02f2cd608295128e1ab082f75f971d7dc53a
Nathan Van Gulck <vangulcknathan@gmail.com>
2020-08-12 14:37:33 +0200

    another merge

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const tabs = document.getElementsByClassName("tab");
 const slider = document.getElementsByTagName("header")[0];
-const tab_buttons = document.getElementsByClassName("tab-button");
+/*const tab_buttons = document.getElementsByClassName("tab-button");*/
 const leftBtn = document.getElementById("leftBtn");
 const rightBtn = document.getElementById("rightBtn");
-const images = ["Images/Zorb1.jpeg","Images/Zorb2.jpg","Images/Zorb3.jpeg"];
+/*const images = ["Images/Zorb1.jpeg","Images/Zorb2.jpg","Images/Zorb3.jpeg"];*/
+const images = document.getElementsByClassName("mySlides");
+
+images[0].style.display = "block";
 let index = 0;
+/*
 let interval = setInterval(()=>{
     rightBtn.click();
-}, 3000);
+}, 3000);*/
 
 leftBtn.addEventListener("click", () => {
     index++;
     if(index === images.length){
         index = 0;
     }
-    slider.style.backgroundImage = images[index];
+    /*slider.style.backgroundImage = images[index];*/
+    Array.from(images).forEach(image => image.style.display = "none");
+    images[index].style.display = "block";
+
 })
 
 rightBtn.addEventListener("click", () => {
@@ -22,9 +29,14 @@ rightBtn.addEventListener("click", () => {
     if(index < 0){
         index = images.length - 1;
     }
-    slider.style.backgroundImage = images[index];
+    /*slider.style.backgroundImage = images[index];*/
+    Array.from(images).forEach(image => {
+        image.style.display = "none"
+    });
+    images[index].style.display = "block";
 })
 
+/*
 tab_buttons.addEventListener("click", e => {
     let target = e.target;
     let showTab = target.getAttribute("data-attribute");
@@ -32,4 +44,16 @@ tab_buttons.addEventListener("click", e => {
         tab.style.display = "hidden";
     });
     showTab.style.display = "block";
-})
+})*/
+
+function Counter(){
+    let countDiv = document.getElementById("counter");
+    let countTo = countDiv.innerText;
+    let counter = 0;
+    let countInterval = setInterval(() => {
+
+        if(counter === countTo){
+            clearInterval(countInterval)
+        }
+    })
+}
